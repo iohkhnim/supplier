2db6c6ffbe2b4b5cbb577784acda1064c5743a01
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-12 16:21:31 +0700

    connect db

--- src/main/java/com/khoi/supplier/SupplierApplication.java
+++ src/main/java/com/khoi/supplier/SupplierApplication.java
@@ -1,10 +1,18 @@
 package com.khoi.supplier;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SupplierApplication {
+
+  @PostConstruct
+  public void init() {
+    // Setting Spring Boot SetTimeZone
+    TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(SupplierApplication.class, args);
