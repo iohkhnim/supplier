4eca17e204fea9c3d1da343c6697628a6e8cb78e
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-14 14:20:08 +0700

    modify code

--- src/main/java/com/khoi/supplier/dao/dao/impl/SupplierDAOImpl.java
+++ src/main/java/com/khoi/supplier/dao/dao/impl/SupplierDAOImpl.java
@@ -3,7 +3,6 @@ package com.khoi.supplier.dao.dao.impl;
 import com.khoi.basecrud.dao.dao.impl.BaseDAOImpl;
 import com.khoi.supplier.dao.ISupplierDAO;
 import com.khoi.supplier.dto.Supplier;
-import com.khoi.supplier.service.ISupplierService;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
