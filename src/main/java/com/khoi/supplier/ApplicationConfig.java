a037d2d5b646df432c52f5aafd619a1113cde976
iohnkhnim <hiepsi1211@gmail.com>
2019-03-25 23:43:43 +0700

    add ssl authentication

--- src/main/java/com/khoi/supplier/ApplicationConfig.java
+++ src/main/java/com/khoi/supplier/ApplicationConfig.java
@@ -2,19 +2,29 @@ package com.khoi.supplier;
 
 import com.khoi.productproto.ProductServiceGrpc;
 import io.grpc.Channel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NegotiationType;
+import io.grpc.netty.NettyChannelBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
 
 @Configuration
 public class ApplicationConfig {
 
   private final String productServiceEndpoint = "localhost:6575";
 
+  private final String productServerKeyPath = "src/main/java/com/khoi/supplier/key/ca.crt";
+
   @Bean(name = "productChannel")
-  Channel productChannel() {
-    return ManagedChannelBuilder.forTarget(productServiceEndpoint).usePlaintext().build();
+  Channel productChannel() throws Exception {
+    return NettyChannelBuilder.forTarget(productServiceEndpoint)
+        .negotiationType(NegotiationType.TLS)
+        .sslContext(
+            GrpcSslContexts.forClient().trustManager(new File(productServerKeyPath)).build())
+        .build();
   }
 
   @Bean(name = "productService")
